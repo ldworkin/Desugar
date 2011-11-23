d0bbad4812df0395a66f16bccd47f880903f28d1
Lili Dworkin <lilidworkin@gmail.com>
2011-11-23 15:56:24 -0500

    removed header comment

--- desugar.py
+++ desugar.py
@@ -1,39 +1,5 @@
 #!/usr/bin/env python
 
-'''
-DESCRIPTION:
-
-A tiny Python script to expand or "desugar" Haskell do blocks. 
-
-You can also ask lambdabot on the IRC channel to @undo expressions;
-this was written mostly for my own understanding, and with the
-intention of expanding the functionality later.
-
-As of now, the capabilities are very limited. Most notably,
-you cannot use pure code in the do blocks (such as let statements). 
-
-DEPENDENCIES:
-
-Pyparsing (http://pyparsing.wikispaces.com/)
-
-USAGE:
-
-Provide the do block as the first (and only) command line argument. 
-If no arguments are provided, you will be prompted for input.
-
-EXAMPLES:
-
-do { x1 <- eval x; x2 <- eval y; safediv x1 x2 } ==
-eval x >>= (\\x1 -> eval y >>= (\\x2 -> safediv x1 x2))
-
-do { x <- xs; y <- ys; return (x,y) } ==
-xs >>= (\\x -> ys >>= (\\y -> return (x,y)))
-
-do { x <- getChar ; y <- getChar; print [x,y]; return True } ==
-getChar >>= (\\x -> getChar >>= (\\y -> print [x,y] >> return True))
-
-'''
-
 import sys
 from pyparsing import *
 
